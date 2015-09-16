d7f02cc752b82be05f23de2f86fb874be5e16ee3
vmugue <vmugue@gmail.com>
2015-09-17 00:23:40 +0200

    just started

---
+++ Peer1.R
@@ -0,0 +1,7 @@
+## The aim of peer assignements is research of movement data, collected with 
+## 5 minutes intervals during 2 months
+## data available on the course page
+data_source <- "https://d396qusza40orc.cloudfront.net/repdata%2Fdata%2Factivity.zip"
+#download.file(data_source, "data_file.zip")
+#unzip("data_file.zip")
+read.csv("data")
